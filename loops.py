e3ba6513d54aa4aac9de390ef50050ac9cc7d1d7
ABHISHEK JAJIMOGGALA <abhishekjajimoggala@gmail.com>
2026-05-27 22:44:39 +0530

    Add files via upload
    Types of loops:
    1)for
    2)while
    3)break
    4)continue
    5)range

---
+++ loops.py
@@ -0,0 +1,129 @@
+#---------------------Loops
+#for,while,pass,break,continue,range
+'''a=[10,20,30,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,50]
+for i in a:
+    print(i,end=" ")'''
+
+'''a=[10,20,30,50]
+for i in a:
+    print(i)
+    print(a)
+    print(type(a))'''
+
+'''a=[10,20,30,50]
+for i in a:
+    print(i)
+print(a)
+print(type(a))'''
+
+'''a=(10,20,30,50)
+for i in a:
+    print(i)'''
+'''a=(10,20,30,50)
+for i in a:
+    print(i,end=",")'''
+
+'''a=(10,20,30,50)
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(a))'''
+'''a={10,20,30,50}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(a))'''
+'''a={'name':'abhishek','month':'may','date':24,'year':2004}
+for i in a.keys():
+    print(i)
+    print(type(a))
+    print(type(a))'''
+
+'''a={'name':'abhishek','month':'may','date':24,'year':2004}
+for i in a.values():
+    print(i)
+    print(type(a))
+    print(type(i))'''    
+
+'''a={'name':'abhishek','month':'may','date':24,'year':2004}
+for i in a.items():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a="codegnan"
+for i in a:
+    print(i)'''
+
+'''a="codegnan"
+for i in a:
+    print(i)
+    print(type(i))
+    print(type(a))'''
+'''a="codegnan"
+for i in a:
+    print(i,end=" ")
+    print(type(i))
+    print(type(a))'''
+
+'''a=[3.4,5.6,8.9]
+for i in a:
+    print(i,end=" ")
+    print(type(i))
+    print(type(a))'''
+
+'''a=[3.4,5.6,8.9]
+for i in a:
+    print(i,end=" ")
+    print(type(i))
+    print(type(a))'''
+
+'''a=[3+4j,3j]
+for i in a:
+    print(i,end=" ")
+    print(type(i))
+    print(type(a))'''
+
+'''fruits=["apple","banana","mango"]
+for i in fruits:
+    print(i)
+    print(type(fruits))
+    print(type(i))'''
+'''a=["codegnan","python","course"]
+b=[]
+for i in a:
+    print(i.upper(),end=",")'''
+
+'''a=["codegnan","python","course"]
+b=[]
+for i in a:
+    b.append(i.upper())
+print(b)'''    
+
+'''a=["codegnan","python","course"]
+for i in a:
+    print(i.upper(),end=" ")'''    
+
+'''a=["codegnan","python","course"]
+b=[]
+for i in a:
+    b.append(i.upper())
+print(b)'''
+
+'''a=["codegnan","python","course"]
+b=[]
+for i in a:
+    b.append(i.upper())
+    print(b)'''
+
+marks=[70,90,80,47]
+sum=0
+for i in marks:
+    sum+=i
+print("Total marks:",sum)    
+    
+    
---
+++ loops_practice.py
@@ -0,0 +1,92 @@
+#....................for loop.......................
+#Practice for loop
+'''a=[10,20,30,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,50]
+for i in a:
+    print(i,end=" ")'''
+
+'''a=[10,20,30,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[12.5,3.5,3.9]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''name=["abhishek","jajimoggala"]
+for i in name:
+    print(i)
+    print(type(i))
+    print(type(name))'''
+
+'''s="abhishek"
+for i in s:
+    print(i)
+print(type(s))
+print(type(i))'''
+
+'''s="abhishek"
+for i in s:
+    print(i,end=",")
+    print(type(s))
+    print(type(i))'''
+
+'''a={20,30,40,40,20,10}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a={20.3,30,40.5,40,20.2,10}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''student={"name":'abhishek',"branch":"cse","course":"python"}
+for i in student.keys():
+    print(i)'''
+
+'''student={"name":'abhishek',"branch":"cse","course":"python"}
+for i in student.values():
+    print(i)'''
+
+'''student={"name":'abhishek',"branch":"cse","course":"python"}
+for i in student.items():
+    print(i)
+    print(type(i))
+    print(type(student))'''
+
+'''a=[2+4j,4+4j,39+4j]
+for i in a:
+    print(i)
+    print(type(i))
+    print(type(a))'''
+
+'''a=(2+4j,4+4j,39+4j)
+for i in a:
+    print(i)
+    print(type(i))
+    print(type(a))'''
+
+#task-1
+'''a=["codegnan","python",'course']
+b=[]
+for i in a:
+    b.append((i).upper())
+print(b)'''
+
+#task-2
+marks=[20,30,10,70,60,50,80]
+sum=0
+for i in marks:
+    sum=sum+i
+print("Marks is:",sum)    
+    
